74505884fdb1c104f6e4bdc8194d6bebb3e18fd7
Nahuel Baglietto <37049113+nbaglietto@users.noreply.github.com>
2024-03-14 23:59:33 -0300

    Create install.sh
    script para automatizar instalacion de docker y kubernetes

---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Configurar los repositorios de Docker y Kubernetes
+
+# Descargar la clave GPG para Docker
+wget -O - https://download.docker.com/linux/ubuntu/gpg > ./docker.key
+gpg --no-default-keyring --keyring ./docker.gpg --import ./docker.key
+gpg --no-default-keyring --keyring ./docker.gpg --export > ./docker-archive-keyring.gpg
+sudo mv ./docker-archive-keyring.gpg /etc/apt/trusted.gpg.d/
+
+# Agregar el repositorio de Docker e instalar Docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install git wget curl -y
+sudo apt install -y docker-ce
+
+# Instalar cri-dockerd para soporte Docker
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//g')
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xvf cri-dockerd-${VER}.amd64.tgz
+sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+# Agregar la clave GPG para Kubernetes
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# Agregar el repositorio de Kubernetes
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Actualizar el repositorio
+sudo apt-get update
+
+# Instalar paquetes de Kubernetes
+sudo apt-get install -y kubelet kubeadm kubectl
+
+# Mantener las versiones para que no se actualicen accidentalmente
+sudo apt-mark hold docker-ce kubelet kubeadm kubectl
+
+# Habilitar el puente de iptables
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sudo sysctl --system
+
+# Inicializar el clúster (en el servidor maestro de Kube)
+sudo kubeadm init --apiserver-advertise-address=192.168.1.180 --cri-socket unix:///var/run/cri-dockerd.sock --pod-network-cidr=192.168.0.0/16
+
+# Configurar el acceso al clúster para el usuario actual
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Configurar la red Calico
+kubectl create -f https://docs.projectcalico.org/v3.25/manifests/calico.yaml
+
+# Comprobar los nodos
+kubectl get nodes
